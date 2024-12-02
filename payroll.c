0cb7e8ff4716926a5d6b6fbaaa46e7ae6bbd0f5a
AlexL-32 <alejandro.n.lopez@wsu.edu>
2024-12-02 02:53:04 -0800

    Update payroll.c

--- payroll.c
+++ payroll.c
@@ -2,5 +2,14 @@
 
 double compute_payment(Employee* emp_ptr)
 {
+	double expected_hours = 80.0;
 
+	if (emp_ptr->title == 'M')
+	{
+		double overtime_rate = 1.8;
+	}
+	else
+	{
+		double overtime_rate = 1.5;
+	}
 }
