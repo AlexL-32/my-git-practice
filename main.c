22392468361192998adb3e636011cf3dbf5a1014
AlexL-32 <alejandro.n.lopez@wsu.edu>
2024-12-02 02:53:46 -0800

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,22 @@
 int main(void)
 {
 	Employee payroll[200];
+	
 
 	FILE* infile = fopen("payroll.txt", "r");
 	FILE* outfile = fopen("paid.txt", "w");
 
 
+	char line[200];
+
+	while (fgets(line, sizeof(line), infile))
+	{
+		Employee* emp = &payroll[200];
+
+
+
+
+	}
 
 	fclose(infile);
 	fclose(outfile);
